740dcd285b45e4e41de1cee4a06af5ef112ab3da
DhruvKumar Nagar <iamdhruvnagar@gmail.com>
2023-07-05 00:27:02 +0530

    Added API for signin with JWT

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,7 +1,7 @@
 const { UserRepository } = require("../repostories");
 const { AppError } = require("../utils/errors/app-error");
 const { StatusCodes } = require("http-status-codes");
-
+const { Auth } = require("../utils/common");
 const userRepo = new UserRepository();
 
 async function create(data) {
@@ -27,6 +27,32 @@ async function create(data) {
   }
 }
 
+async function signin(data) {
+  try {
+    const user = await userRepo.getUserByEmail(data.email);
+    if (!user) {
+      throw new AppError(
+        "No user found with given email ID",
+        StatusCodes.NOT_FOUND
+      );
+    }
+    const passwordMatch = Auth.checkPassword(data.password, user.password);
+    if (!passwordMatch) {
+      throw new AppError("Invalid Password", StatusCodes.BAD_REQUEST);
+    }
+    const jwt = Auth.createToken({ id: user.id, email: user.email });
+    return jwt;
+  } catch (error) {
+    if (error instanceof AppError) throw error;
+    console.log(error);
+    throw new AppError(
+      "Something went wrong at the time of doing signin",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
 module.exports = {
   create,
+  signin,
 };
